f2612152f630279789d59b3a355a3539244df675
John_Ballen <jhonn.ballen@gmail.com>
2021-12-19 10:13:17 -0500

    Se muestra la tupla sobre tabla producto organizadamente

--- pythonProject/Consulta.py
+++ pythonProject/Consulta.py
@@ -3,24 +3,20 @@ Este modulo trabaja con fetch one para traer solo de a un registro
 
 """
 import psycopg2
-#Conexion con la base de datos
+
 conn = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='Legos')
 try:
     with conn:
-        #With cierra recurso de cursor, por lo que no es necesario llamar al metodo close()
-        with conn.cursor() as curs:
-            query = 'SELECT * FROM Legos WHERE id IN %s'
-            entrada = input('Proporcina los id\'s a buscar (separado por comas): ')
-            #Split separa cada uno de los valores de el input "entrada, luego lo convierte en tupla
-            #Se convierte en tupla de tuplas, observe la coma al final de la sentencia
-            llavesPrimarias = (tuple(entrada.split(',')),)
 
-            curs.execute(query,llavesPrimarias)
-            #fetchone regresa todos los registros
+        with conn.cursor() as curs:
+            find = "SELECT producto FROM Legos WHERE iniciales IN %s"
+            entrada = input('Indique las iniciales del producto: ')
+            llavesPrimarias = (tuple(entrada.split(',')),)
+            curs.execute(find, llavesPrimarias)
             regs = curs.fetchall()
-            #Iterar cada uno de los resultados del fetch all
             for registro in regs:
-                print(registro)
+                print('Nombre figura: ', registro)
+
 #e guarda error
 except Exception as e:
     print(f'Ocurrio un error: {e}')
---
+++ pythonProject/ConsultaUnidad.py
@@ -0,0 +1,29 @@
+"""
+Este modulo trabaja con fetch one para traer solo de a un registro
+
+"""
+class ConsultaUnidad:
+
+    import psycopg2
+
+    conn = psycopg2.connect(user='postgres',password='admin',host='127.0.0.1',port='5432',database='Legos')
+    try:
+        with conn:
+
+            with conn.cursor() as curs:
+                qProducto = "SELECT * FROM Legos WHERE iniciales = %s"
+                entrada = input('Indique las iniciales del producto: ')
+                curs.execute(qProducto, (entrada,))
+                regs = curs.fetchone()
+                print(f'Nombre de figura: ', regs[3])
+                print(f'Franquicia: ', regs[1])
+                print(f'Codigo: ', regs[2])
+                print(f'Cantidad: ', regs[4])
+                print(f'Ubicación: ', regs[6])
+                print(f'Precio: ', regs[5])
+    #e guarda error
+    except Exception as e:
+        print(f'Ocurrio un error: {e}')
+    finally:
+        conn.close()
+
